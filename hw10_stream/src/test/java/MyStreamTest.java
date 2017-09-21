8bb4eb3866d7a1fcc595b38fee160e882377f281
botwy <botwy@mail.ru>
2017-09-21 12:39:01 +0300

    Stream #4

--- hw10_stream/src/test/java/MyStreamTest.java
+++ hw10_stream/src/test/java/MyStreamTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,9 +13,21 @@ public class MyStreamTest {
     public void TestOf() {
         List<Integer> list = Arrays.asList(1,2,3);
        MyStream<Integer> my_stream = MyStream.of(list);
+
+        Assert.assertNotNull(my_stream);
+
        my_stream.filter(a->a==2);
+
        my_stream.transform(a->3);
        Map map = my_stream.toMap(a->a.toString(),a->a);
+
+        for (Map.Entry<String,Integer> item:((Map<String,Integer>)map).entrySet()
+             ) {
+            Assert.assertTrue("В мапе у всех записей вэлью должен быть равен 3"
+                    ,item.getValue()==3);
+        }
+
+        Assert.assertNotNull(map);
 
        Person person1 = new Person("Bob", 18);
         Person person2 = new Person("Masha", 21);
@@ -26,9 +39,13 @@ public class MyStreamTest {
                 .transform(p->new Person(p.getName(),p.getAge()+30))
                 .toMap(p->p.getName(),p->p);
 
+        Assert.assertNotNull(map_pers);
+
         for (Map.Entry<String,Person> item:map_pers.entrySet()
              ) {
          System.out.println(item.getKey()+" "+item.getValue());
+         Assert.assertTrue("Возраст персон в мапе должен быть больше 50 лет"
+                 ,item.getValue().getAge()>50);
         }
     }
 }
