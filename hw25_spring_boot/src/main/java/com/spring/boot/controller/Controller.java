870df9313cf7aa946fab6fe67ff4f349d005546a
botwy <botwy@mail.ru>
2017-12-22 13:17:07 +0300

    hw30 jms chat and spring boot

---
+++ hw25_spring_boot/src/main/java/com/spring/boot/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.spring.boot.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<String> exampleResponse() {
+        return new ResponseEntity<String>("Horosho", HttpStatus.OK);
+    }
+
+}
